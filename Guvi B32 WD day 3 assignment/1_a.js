dc6f3bf7583ebf79d1e872243118a8f7da37813b
Sameeksha <97214160+Sameedu@users.noreply.github.com>
2022-02-06 22:42:50 +0530

    day 1,2 ,3 updated

---
+++ Guvi B32 WD day 3 assignment/1_a.js
@@ -0,0 +1,13 @@
+var candidate={
+    "name":"Saami",
+    "age":"12",
+    "marks":"distinction",
+    "class":"MBA",
+    "branch":"Health Care"
+}
+arr=[candidate]
+for(var i=0;i<candidate.length;i++);
+{
+    console.log(arr[i]);
+    
+}
---
+++ Guvi B32 WD day 3 assignment/2_b.js
@@ -0,0 +1,14 @@
+var candidate={
+    "name":"Saami",
+    "age":"12",
+    "marks":"distinction",
+    "class":"MBA",
+    "branch":"Health Care"
+}
+
+for (var i in candidate)
+{
+
+    console.log(i, candidate[i]);
+    
+}
