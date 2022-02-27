bbb34000a9af4326391b32621351ede3d833ebad
Sameeksha <97214160+Sameedu@users.noreply.github.com>
2022-02-27 23:59:02 +0530

    day 4

---
+++ WDday4assignment/2.js
@@ -0,0 +1,10 @@
+//Use the rest countries API url -> https://restcountries.eu/rest/v2/all and display all the country flags in console
+var request = new XMLHttpRequest();
+request.open("GET", "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+request.send();
+request.onload = function(){    
+    var data = JSON.parse(request.response);
+    for (var i = 0; i<data.length;i++){
+        console.log(data[i].flag)
+    }
+}
