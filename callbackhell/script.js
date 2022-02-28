d6216a2ef72179878b6d527168230d2c46687631
Sameeksha <97214160+Sameedu@users.noreply.github.com>
2022-02-28 12:30:28 +0530

    callbackhell

---
+++ callbackhell/script.js
@@ -0,0 +1,35 @@
+
+
+let countdown = 10
+window.setTimeout(() => {
+    document.querySelector(".classexample").innerText = --countdown;
+    window.setTimeout(() => {
+        document.querySelector(".classexample").innerText = --countdown;
+        window.setTimeout(() => {
+            document.querySelector(".classexample").innerText = --countdown;
+            window.setTimeout(() => {
+                document.querySelector(".classexample").innerText = --countdown;
+                window.setTimeout(() => {
+                    document.querySelector(".classexample").innerText = --countdown;
+                    window.setTimeout(() => {
+                        document.querySelector(".classexample").innerText = --countdown;
+                        window.setTimeout(() => {
+                            document.querySelector(".classexample").innerText = --countdown;
+                            window.setTimeout(() => {
+                                document.querySelector(".classexample").innerText = --countdown;
+                                    window.setTimeout(() => {
+                                    document.querySelector(".classexample").innerText = --countdown;
+                                        window.setTimeout(() => {
+                                        document.querySelector(".classexample").innerText = "Happy Independence day!"}
+                                        ,1000)
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                }, 1000);
+            }, 1000); 
+        }, 1000);
+    }, 1000);
+
+    
